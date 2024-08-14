f047e8ab2be19f0890822f07261d86fd9dc4c148
LeftMast <nkpvakatov@mail.ru>
2024-08-14 13:16:19 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+print(12 + 89 - 1)
+print(100)
+
+# "1st program".
+print ( 9 ** 0.5 ),
+print ( 3 * 5 )
+
+#"2nd program"
+print( 9.99 > 9.98 and 1000 != 1000.1 )
+
+#"3rd program".
+print ( 2* 2 +2 )
+print (2 * ( 2 + 2))
+print ( 6 == 8 )
+
+#"4rd program".
+print ( '123.456')
+print ( '123.456' or 123.456  )
+print ( 123.456 * 10 )
+print (int(1234.56 // 10 ))
