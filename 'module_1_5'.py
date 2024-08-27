0f277adc885e9e25be2eafd29869682bceee4634
LeftMast <nkpvakatov@mail.ru>
2024-08-27 10:50:00 +0800

    Update 'module_1_5'.py

--- 'module_1_5'.py
+++ 'module_1_5'.py
@@ -1,15 +1,21 @@
-from lib2to3.fixes.fix_tuple_params import tuple_name
-
-immutable_var = 1,2,3, True, 'a','b', 'Urban'
-list_ = [1,2,3, True, 'a','b',]
-print(immutable_var.__sizeof__())
-print(list_.__sizeof__())
-immutable_var_2 = (1,2,3,4)
-immutable_var_3 = tuple([1,2,3,4])
+immutable_var = (29, 'string', True, {'key': 'value'})
 print(immutable_var)
-print(immutable_var_2)
-print(immutable_var_3)
-print(type(immutable_var))
-print(immutable_var [0])
-immutable_var [0] = 200
-
+str_ = 'chars'
+print(id(immutable_var[1]))
+print(id(str_))
+#immutable_var[1] = str_
+''' "TypeError: 'tuple' object does not support item assignment"
+  Контейнеры в Python хранят только ссылки на объекты
+ и могут гарантровать только их целостность, но не их содержимое.
+ А в данном примере кода необходимо поменять ссылку обекта 'string' на другой объект 'chars'
+  Но если объект остается тот же то все получится. Как в примере строкой ниже'''
+print(id(immutable_var[3]))
+immutable_var[3]['key'] = 'str_'
+print(id(immutable_var[3]))
+print(immutable_var)
+print('''------------------------------------------------
+Вывод для списка''')
+mutable_list = ['new string', False, 29, {'key_list': 'value_list'}]
+print(mutable_list)
+mutable_list[:2] = 'chars', True
+print(mutable_list)
