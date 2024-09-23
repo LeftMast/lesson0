37ede23647808c63201f644140253b9fc0912a14
LeftMast <nkpvakatov@mail.ru>
2024-09-23 17:58:35 +0800

    Add files via upload

---
+++ module_4/fake_math.py
@@ -0,0 +1,13 @@
+def main():
+    pass
+
+
+def divide(first, second):
+    if second == 0:
+        return 'Ошибка'
+    else:
+        return first / second
+
+
+if __name__ == '__main__':
+    main()
---
+++ module_4/true_math.py
@@ -0,0 +1,16 @@
+from math import inf
+
+
+def main():
+    pass
+
+
+def divide(first, second):
+    if second == 0:
+        return inf
+    else:
+        return first / second
+
+
+if __name__ == '__main__':
+    main()
