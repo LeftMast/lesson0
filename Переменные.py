9735ae9507df25ff788770b02533daa772c449e5
LeftMast <nkpvakatov@mail.ru>
2024-10-22 16:51:35 +0800

    Update Переменные.py

--- Переменные.py
+++ Переменные.py
@@ -1,6 +1,4 @@
+a = 12
+b = 1.5
 first_name = 'Курс:Python,'
-all_the_tasks = 12
-hours_spent = 1.5
-Average_execution_time = 0.125
-print(hours_spent / all_the_tasks)
-print(first_name,'всего задач:',all_the_tasks,'затрачено часов:',hours_spent,',среднее время выполнения', Average_execution_time,'часа')
+print(first_name,'всего задач:', a ,'затрачено часов:', b ,',среднее время выполнения', b / a  ,'часа')
