71ab229c11b0e355191bf08f87fd10a91126c7d7
LeftMast <nkpvakatov@mail.ru>
2024-10-27 19:40:22 +0800

    Add files via upload

---
+++ module 9/module_9_2.py
@@ -0,0 +1,11 @@
+first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+
+first_result = [len(str_) for str_ in first_strings if len(str_) >= 5]
+second_result = [(first_str, second_str) for first_str in first_strings
+                 for second_str in second_strings if len(first_str) == len(second_str)]
+third_result = {str_: len(str_) for str_ in first_strings + second_strings if not len(str_) % 2}
+
+print(first_result)
+print(second_result)
+print(third_result)
